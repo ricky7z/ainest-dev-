ad056d58e294fad5bc2d1ebc9cbdcd23ea9cf2af
ricky7z <147446927+ricky7z@users.noreply.github.com>
2025-06-26 01:10:59 +0000

    Delete components directory

--- components/admin/admin-client-provider.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { AdminProvider } from "@/contexts/admin-context";
-
-export default function AdminClientProvider({ children }: { children: React.ReactNode }) {
-  return <AdminProvider>{children}</AdminProvider>;
-} 
